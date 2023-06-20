3c20246e9b1104317718b2bedf9b3271e9694974
Andrew Truex <andrewtruex@gmail.com>
2023-06-20 19:52:28 -0400

    changed navbar and added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import AboutSection from "@/components/AboutSection"
+
+const About = () => {
+  return (
+  <div className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+    <AboutSection />
+  </div>
+  )
+}
+
+export default About
--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -86,18 +86,7 @@ const AboutSection = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-row items-center text-center justify-center">
-          <Link
-            to="projects"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            <HiArrowDown size={35} className="animate-bounce" />
-          </Link>
-        </div>
+
       </div>
     </section>
   )
