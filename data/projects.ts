bd22597f7d4bce15c51b4f47dfff3c65fa592c3d
Andrew Truex <andrewtruex@gmail.com>
2023-07-23 15:28:21 -0400

    added project data

---
+++ data/projects.ts
@@ -0,0 +1,75 @@
+export const projects = [
+  {
+    name: "Portfolio",
+    description: "My old personal portfolio website built with Vue.js.",
+    image: "/portfolio2.png",
+    github: "https://github.com/ajtruex/portfolio2",
+    link: "https://beta.andrewtruex.tech/",
+    route: "Portfolio",
+    slug: "portfolio",
+    tools: [
+      { name: "Vue.js", image: "/vue-logo.png" },
+      { name: "Airtable", image: "/airtable-logo.png" },
+      { name: "Bulma", image: "/bulma-logo.png" },
+    ],
+  },
+  {
+    name: "MovieVuer",
+    description: "A movie review aggregator made with Vue.js.",
+    image: "/movie-vuer.png",
+    github: "https://github.com/ajtruex/MovieVuer",
+    link: "https://movievuer.netlify.app/",
+    route: "MovieVuer",
+    slug: "movievuer",
+    tools: [
+      { name: "Vue.js", image: "/vue-logo.png" },
+      { name: "The Movie Database API", image: "/tmdb-full-logo.svg" },
+      { name: "Netlify", image: "/netlify-logo.png" },
+      { name: "Bootstrap", image: "/bootstrap-logo.png" },
+    ],
+    goals: [
+      "Use Vue.js to build full web application",
+      "Use axios to pull information from OMDb API",
+      "Style application with bootstrap",
+    ],
+  },
+  {
+    name: "Next Movies",
+    description: "A movie review aggregator made with Next.js.",
+    image: "/next-movies.png",
+    github: "https://github.com/ajtruex/next-movies",
+    link: "http://movies.andrewtruex.tech/",
+    route: "NextMovies",
+    tools: [
+      { name: "Next.js", image: "/nextjs-logo.png" },
+      { name: "The Movie Database API", image: "/tmdb-full-logo.svg" },
+      { name: "Vercel", image: "/vercel-logo.png" },
+      { name: "Tailwind CSS", image: "/tailwind-logo.png" },
+    ],
+  },
+  {
+    name: "BS Upcycled Home Decor",
+    description:
+      "A website for a local business built with Next.js featuring an image gallery.",
+    image: "/bs2.png",
+    github: "https://github.com/ajtruex/bsupcycledhomedecor",
+    link: "https://bsupcycledhomedecor.com/",
+    route: "bs",
+    tools: [{ name: "Next.js", image: "/nextjs-logo.png" }],
+  },
+  {
+    name: "WhoSampled Raycast Extension",
+    description:
+      "Search WhoSampled.com from the currently playing song in Spotify",
+    image: "/whosampled-raycast-extension.png",
+    github:
+      "https://github.com/raycast/extensions/tree/main/extensions/whosampled",
+    link: "https://www.raycast.com/truex/whosampled",
+    route: "whosampled",
+    tools: [
+      { name: "TypeScript", image: "/typescript-logo.png" },
+      { name: "Raycast", image: "/raycast-logo.png" },
+      { name: "Node.js", image: "/nodejs-logo.svg" },
+    ],
+  },
+]
