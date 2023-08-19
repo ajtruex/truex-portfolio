e737487560e9c2c8f0f03beed0d09088e81c8aee
Andrew Truex <andrewtruex@gmail.com>
2023-08-19 08:47:37 -0400

    add icons to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,33 +5,31 @@ const Contact = () => {
   const handleSocialMediaClick = (url: string) => {
     window.open(url, "_blank")
   }
-  return (
-    <>
-      <div>
-        <h1>Contact Us</h1>
-        <p>
-          Feel free to reach out to us on any of our social media platforms:
-        </p>
-        <ul>
-          <li>
-            <Link href="https://twitter.com">
-              <a target="_blank">Twitter</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://facebook.com">
-              <a target="_blank">Facebook</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://instagram.com">
-              <a target="_blank">Instagram</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  )
+  return <>
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        Feel free to reach out to us on any of our social media platforms:
+      </p>
+      <ul>
+        <li>
+          <Link href="https://twitter.com" target="_blank">
+            Twitter
+          </Link>
+        </li>
+        <li>
+          <Link href="https://facebook.com" target="_blank">
+            Facebook
+          </Link>
+        </li>
+        <li>
+          <Link href="https://instagram.com" target="_blank">
+            Instagram
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </>;
 }
 
 export default Contact
