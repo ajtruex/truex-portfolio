b6431fbe881cf8dcce40997a82a67a2987095cb7
Andrew Truex <andrewtruex@gmail.com>
2023-08-25 00:47:58 -0400

    finish navbar for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,32 +54,26 @@ const Navbar = () => {
           </div>
         </Link>
         <div className="md:hidden ">
-          <button
-            onClick={() => setNavbar(!navbar)}
-            className={`
-
-          ${navbar ? "ml-24 mr-8" : "ml-0 mr-0"}
-          `}
-          >
+          <button onClick={() => setNavbar(!navbar)}>
             {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
           </button>
         </div>
       </div>
 
       <div
-        className={`fixed z-10 cursor-pointer flex-row items-center md:rounded-full rounded-xl border bg-white bg-opacity-95 px-6 py-2 shadow-xl backdrop-blur-sm backdrop-filter dark:border-slate-100/10 dark:bg-black dark:bg-opacity-75 lg:left-1/2 md:flex lg:-translate-x-1/2 lg:transform ml-12 top-8 left-1/4 ${
+        className={`md:fixed z-10 cursor-pointer flex-row items-center md:rounded-full rounded-xl border bg-white bg-opacity-95 px-6 py-2 shadow-xl backdrop-blur-sm backdrop-filter dark:border-slate-100/10 dark:bg-black md:dark:bg-opacity-75 dark:bg-opacity-90 lg:left-1/2 md:flex lg:-translate-x-1/2 lg:transform md:ml-12 top-24 md:left-1/4 inset-x-0 md:inset-x-auto absolute left-0 md:top-8 ${
           navbar ? "block" : "hidden"
         } md:block`}
       >
-        <div className="flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0">
-          <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+        <div className="flex-1 justify-self-center pb-3 mt-2 md:block md:pb-0 md:mt-0">
+          <div className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0">
             {NAV_ITEMS.map((item, idx) => {
               return (
                 <Link
                   key={idx}
                   href={item.page}
                   className={
-                    "block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100"
+                    "block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100 text-center"
                   }
                   onClick={handleLinkClick}
                 >
@@ -90,14 +84,14 @@ const Navbar = () => {
             {currentTheme === "dark" ? (
               <button
                 onClick={() => setTheme("light")}
-                className="bg-slate-100 p-2 rounded-xl md:ml-0 ml-2.5"
+                className="bg-slate-100 p-2 rounded-xl md:mx-0 mx-auto flex"
               >
                 <RiSunLine size={25} color="black" />
               </button>
             ) : (
               <button
                 onClick={() => setTheme("dark")}
-                className="bg-slate-100 p-2 rounded-xl text-black md:ml-0 ml-2.5"
+                className="bg-slate-100 p-2 rounded-xl text-black md:mx-0 mx-auto flex"
               >
                 <RiMoonFill size={25} />
               </button>
