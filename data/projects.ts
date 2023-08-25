51b8f1678b57d5f5f1d86531cdd7688becfe9916
Andrew Truex <andrewtruex@gmail.com>
2023-08-25 02:46:40 -0400

    change projects order

--- data/projects.ts
+++ data/projects.ts
@@ -1,5 +1,50 @@
 export const projects = [
   {
+    name: "Next Movies",
+    description: "A movie review aggregator made with Next.js.",
+    image: "/next-movies.png",
+    github: "https://github.com/ajtruex/next-movies",
+    link: "http://movies.andrewtruex.tech/",
+    route: "NextMovies",
+    tools: [
+      { name: "Next.js", image: "/nextjs-logo.png" },
+      { name: "The Movie Database API", image: "/tmdb-full-logo.svg" },
+      { name: "Vercel", image: "/vercel-logo.png" },
+      { name: "Tailwind CSS", image: "/tailwind-logo.png" },
+    ],
+  },
+  {
+    name: "BS Upcycled Home Decor",
+    description:
+      "A website for a local business built with Next.js featuring an image gallery.",
+    image: "/bs2.png",
+    github: "https://github.com/ajtruex/bsupcycledhomedecor",
+    link: "https://bsupcycledhomedecor.com/",
+    route: "bs",
+    tools: [
+      { name: "Next.js", image: "/nextjs-logo.png" },
+      { name: "Vercel", image: "/vercel-logo.png" },
+      { name: "Tailwind CSS", image: "/tailwind-logo.png" },
+      { name: "Cloudinary", image: "/cloudinary-logo.png" },
+      { name: "Framer Motion", image: "/framer-motion-logo.png" },
+    ],
+  },
+  {
+    name: "WhoSampled Raycast Extension",
+    description:
+      "Search WhoSampled.com from the currently playing song in Spotify",
+    image: "/whosampled-raycast-extension.png",
+    github:
+      "https://github.com/raycast/extensions/tree/main/extensions/whosampled",
+    link: "https://www.raycast.com/truex/whosampled",
+    route: "whosampled",
+    tools: [
+      { name: "TypeScript", image: "/typescript-logo.png" },
+      { name: "Raycast", image: "/raycast-logo.png" },
+      { name: "Node.js", image: "/nodejs-logo.svg" },
+    ],
+  },
+  {
     name: "Portfolio",
     description: "My personal portfolio website built with Next.js.",
     image: "/next-portfolio.png",
@@ -44,51 +89,6 @@ export const projects = [
       "Use Vue.js to build full web application",
       "Use axios to pull information from OMDb API",
       "Style application with bootstrap",
-    ],
-  },
-  {
-    name: "Next Movies",
-    description: "A movie review aggregator made with Next.js.",
-    image: "/next-movies.png",
-    github: "https://github.com/ajtruex/next-movies",
-    link: "http://movies.andrewtruex.tech/",
-    route: "NextMovies",
-    tools: [
-      { name: "Next.js", image: "/nextjs-logo.png" },
-      { name: "The Movie Database API", image: "/tmdb-full-logo.svg" },
-      { name: "Vercel", image: "/vercel-logo.png" },
-      { name: "Tailwind CSS", image: "/tailwind-logo.png" },
-    ],
-  },
-  {
-    name: "BS Upcycled Home Decor",
-    description:
-      "A website for a local business built with Next.js featuring an image gallery.",
-    image: "/bs2.png",
-    github: "https://github.com/ajtruex/bsupcycledhomedecor",
-    link: "https://bsupcycledhomedecor.com/",
-    route: "bs",
-    tools: [
-      { name: "Next.js", image: "/nextjs-logo.png" },
-      { name: "Vercel", image: "/vercel-logo.png" },
-      { name: "Tailwind CSS", image: "/tailwind-logo.png" },
-      { name: "Cloudinary", image: "/cloudinary-logo.png" },
-      { name: "Framer Motion", image: "/framer-motion-logo.png" },
-    ],
-  },
-  {
-    name: "WhoSampled Raycast Extension",
-    description:
-      "Search WhoSampled.com from the currently playing song in Spotify",
-    image: "/whosampled-raycast-extension.png",
-    github:
-      "https://github.com/raycast/extensions/tree/main/extensions/whosampled",
-    link: "https://www.raycast.com/truex/whosampled",
-    route: "whosampled",
-    tools: [
-      { name: "TypeScript", image: "/typescript-logo.png" },
-      { name: "Raycast", image: "/raycast-logo.png" },
-      { name: "Node.js", image: "/nodejs-logo.svg" },
     ],
   },
 ]
