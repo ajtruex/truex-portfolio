8be94b879aa264489c3e099bd6edf05bf4d5b157
Andrew Truex <andrewtruex@gmail.com>
2023-08-31 20:52:21 -0400

    add spotify and last.fm links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,6 +9,7 @@ import {
   FiMail,
 } from "react-icons/fi"
 import { FaDev } from "react-icons/fa"
+import { SiLastdotfm, SiSpotify } from "react-icons/si"
 import Icon from "@/components/Icon"
 
 const Contact = () => {
@@ -23,7 +24,7 @@ const Contact = () => {
               target="_blank"
               className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-violet-900 rounded-[30px] px-[10px] py-2 text-4xl"
             >
-              <FiGithub className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
+              <FiGithub className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
               Github
             </Link>
             <Link
@@ -31,7 +32,7 @@ const Contact = () => {
               target="_blank"
               className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-violet-800 rounded-[30px] px-[10px] py-2 text-4xl"
             >
-              <FiTwitter className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
+              <FiTwitter className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
               Twitter
             </Link>
             <Link
@@ -59,21 +60,37 @@ const Contact = () => {
               target="_blank"
               className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-violet-500 rounded-[30px] px-[10px] py-2 text-4xl"
             >
-              <FaDev className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
+              <FaDev className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
               Dev.to
+            </Link>
+            <Link
+              href="https://www.last.fm/user/ajtruex"
+              target="_blank"
+              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-purple-800 rounded-[30px] px-[10px] py-2 text-4xl"
+            >
+              <SiLastdotfm className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
+              Last.fm
+            </Link>
+            <Link
+              href="https://open.spotify.com/user/andrewtruex"
+              target="_blank"
+              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-purple-700 rounded-[30px] px-[10px] py-2 text-4xl"
+            >
+              <SiSpotify className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
+              Spotify
             </Link>
             <Link
               href="https://www.youtube.com/@andrewtruex"
               target="_blank"
-              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-[56px]/[60px] tracking-tight font-black hover:bg-purple-900 rounded-[30px] px-[10px] py-2 text-4xl"
+              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-[56px]/[60px] tracking-tight font-black hover:bg-purple-600 rounded-[30px] px-[10px] py-2 text-4xl"
             >
-              <FiYoutube className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
+              <FiYoutube className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
               YouTube
             </Link>
             <Link
               href="https://www.instagram.com/atruedev/"
               target="_blank"
-              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-[50px]/[60px] tracking-tight font-black hover:bg-purple-800 rounded-[30px] px-[10px] py-2 text-4xl"
+              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-[50px]/[60px] tracking-tight font-black hover:bg-purple-500 rounded-[30px] px-[10px] py-2 text-4xl"
             >
               <FiInstagram className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
               Instagram
@@ -81,9 +98,9 @@ const Contact = () => {
             <Link
               href="mailto:andrewtruex@gmail.com"
               target="_blank"
-              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-purple-700 rounded-[30px] px-[10px] py-2 text-4xl"
+              className="text-neutral-100 dark:text-neutral-100 flex items-center lg:text-6xl font-black hover:bg-purple-400 rounded-[30px] px-[10px] py-2 text-4xl"
             >
-              <FiMail className="lg:text-6xl lg:mr-48 mr-24 text-4xl " />
+              <FiMail className="lg:text-6xl lg:mr-48 mr-24 text-4xl" />
               Email
             </Link>
           </div>
