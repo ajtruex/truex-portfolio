d65f087b5b822b5180cf73f0116d0f70f8c67244
Andrew Truex <andrewtruex@gmail.com>
2023-09-05 07:31:03 -0400

    re-add env variable

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -4,7 +4,7 @@ import { SiSpotify } from "react-icons/si"
 
 async function getNpOrRpSong() {
   const res = await fetch(
-    "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ajtruex&api_key=22b72a9335e28b31e18527b6122ce723&format=json&limit=2",
+    "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ajtruex&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=2",
     {
       headers: {
         "Content-Type": "application/json",
