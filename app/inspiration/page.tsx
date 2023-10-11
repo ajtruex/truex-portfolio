10078d1f456cc1b4dd54b2c3b8c09c9b6905b724
Andrew Truex <andrewtruex@gmail.com>
2023-10-10 22:30:05 -0400

    use video links

--- app/inspiration/page.tsx
+++ app/inspiration/page.tsx
@@ -4,18 +4,27 @@ export default function Inspiration() {
       <h1 className="text-5xl sm:text-9xl text-center mt-12 font-garamond">
         Inspiration
       </h1>
-
       <video
         controls
         poster="/kanye-poster.png"
         className="rounded-xl -rotate-6 my-12 mb-20 sm:mb-0"
       >
         <source
-          src="https://burger.bunkr.ru/2008-Grammy-Awardsv2-IIdSu1rP.mp4"
+          src="https://d34073qwlt06j3.cloudfront.net/KanyeWestPerformsStrongerAndHeyMama.mp4"
           type="video/mp4"
         />
       </video>
 
+      {/* <video
+        controls
+        poster="/kanye-poster.png"
+        className="rounded-xl -rotate-6 my-12 mb-20 sm:mb-0"
+      >
+        <source
+          src="https://player.odycdn.com/v6/streams/62736b213f015ac31d27ab57f66355d6b583ac94/91435b"
+          type="video/mp4"
+        />
+      </video> */}
       <div className="flex flex-col">
         <h1 className="text-5xl sm:text-8xl font-bold items-end text-end justify-end mt-12 tracking-tight font-seasons">
           Memento Mori
@@ -26,7 +35,18 @@ export default function Inspiration() {
       </div>
       <video controls width="500" className="rounded-xl rotate-3 my-12 mr-96">
         <source
-          src="https://res.cloudinary.com/ajtruex/video/upload/v1696464400/rapisreligion2023-08-21__021616.mov"
+          src="https://res.cloudinary.com/ajtruex/video/upload/q_auto:best/v1696464400/rapisreligion2023-08-21__021616.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <video
+        controls
+        poster="/ChrisCole-NewBlood.png"
+        width="1440"
+        height="1080"
+      >
+        <source
+          src="https://d34073qwlt06j3.cloudfront.net/ChrisCole-NewBlood.mp4"
           type="video/mp4"
         />
       </video>
