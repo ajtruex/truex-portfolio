0ef4b35e3bf44cf1c32811fe8109c8c0a89e9719
Andrew Truex <andrewtruex@gmail.com>
2024-04-28 06:40:20 -0400

    add dropdown to toggle month/year on spotify

--- app/spotify/page.tsx
+++ app/spotify/page.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 // import Layout from "../components/Layout";
+// "use client"
 import NowPlaying from "@/components/NowPlaying"
 import TopArtists from "@/components/TopArtists"
 import TopTracks from "@/components/TopTracks"
+// import Tip from "@/components/Tip"
 
 export default function Spotify() {
   return (
     <div className="min-h-screen h-full p-8 flex flex-col md:mx-auto mx-1 font-circular">
-      <section className="flex flex-col justify-between mt-16 md:mt-0 prose gap-4">
-        <div>
+      <section className="flex flex-col  mt-16 md:mt-0 prose gap-4">
+        <div
+          className="flex flex-col md:w-1/2
+        w-full mx-auto"
+        >
+          {/* <Tip /> */}
           <h1 className="dark:text-zinc-200 text-zinc-900 leading-none mb-3 font-bold text-5xl">
             Spotify Stats
           </h1>
@@ -16,9 +22,17 @@ export default function Spotify() {
             My Spotify stats
           </p>
         </div>
-        <NowPlaying />
+        <div
+          className="flex flex-col lg:w-1/2 md:w-auto
+        w-full mx-auto"
+        >
+          <NowPlaying />
+        </div>
+
+        {/* <div className="flex flex-row w-full"> */}
         <TopArtists />
         <TopTracks />
+        {/* </div> */}
       </section>
     </div>
   )
--- components/TopArtists.tsx
+++ components/TopArtists.tsx
@@ -2,6 +2,8 @@
 import useSWR from "swr"
 import fetcher from "../lib/fetcher"
 import Link from "next/link"
+import { useState } from "react"
+import { FiChevronDown } from "react-icons/fi"
 
 export type TopArtistsResponse = {
   name: string
@@ -10,16 +12,105 @@ export type TopArtistsResponse = {
 }
 
 export default function TopArtists() {
+  const [to, setTo] = useState("Month")
+  const [isOpen, setIsOpen] = useState(false)
   const { data } = useSWR<TopArtistsResponse[]>("/api/top-artists", fetcher)
   const { data: year } = useSWR<TopArtistsResponse[]>(
     "/api/top-artists-year",
     fetcher
   )
+
   return (
     <div className="flex space-x-0 md:space-x-4 md:flex-row flex-col md:space-y-0 space-y-4 justify-center items-center">
-      <div className="flex flex-col justify-between md:w-1/2 w-full gap-2 p-4 rounded-lg shadow-xl dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200">
-        {/* <div> */}
-        {/* <div className="flex flex-col justify-between gap-2 p-4 mb-6 rounded-lg shadow-xl dark:bg-gradient-to-r dark:from-neutral-800 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200"> */}
+      <div className="flex flex-col justify-between gap-2 p-4 rounded-lg shadow-xl dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200 md:w-1/2 w-full">
+        <div>
+          <h2 className="m-0 text-xl font-black dark:text-zinc-200 text-zinc-900">
+            Top Artists
+          </h2>
+          <div className="relative">
+            <button
+              className="bg-zinc-800 p-2 rounded hover:bg-zinc-800 text-sm dark:text-zinc-400 text-zinc-700"
+              onClick={() => setIsOpen(!isOpen)}
+              value={to}
+            >
+              {to}
+              <FiChevronDown className="inline ml-1" />
+            </button>
+            {isOpen && (
+              <div className="absolute mt-1 m-0 text-sm dark:text-zinc-400 text-zinc-700 rounded p-1 shadow bg-zinc-800">
+                <button
+                  onClick={(
+                    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => (
+                    setTo((e.target as HTMLButtonElement).value),
+                    setIsOpen(false)
+                  )}
+                  className="block w-full p-2 rounded  hover:bg-gray-100"
+                  value={"Month"}
+                >
+                  Month
+                </button>
+                <button
+                  onClick={(
+                    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => (
+                    setTo((e.target as HTMLButtonElement).value),
+                    setIsOpen(false)
+                  )}
+                  className="block w-full p-2 rounded hover:bg-gray-100"
+                  value={"Year"}
+                >
+                  Year
+                </button>
+              </div>
+            )}
+          </div>
+          {to === "Month" ? (
+            <div className="flex flex-col ">
+              {data?.map((artist, index) => (
+                <Link
+                  href={artist.url}
+                  key={index}
+                  className="flex items-center justify-between p-2 duration-200 rounded-lg cursor-pointer dark:hover:bg-zinc-900/60 hover:bg-zinc-100/60 hover:shadow-lg"
+                  target="_blank"
+                >
+                  <p className="m-0 dark:text-zinc-200 text-zinc-900">
+                    {artist.name}
+                  </p>
+                  <p className="m-0 dark:text-zinc-200 text-zinc-900">
+                    {artist.playcount}{" "}
+                    <span className="dark:text-zinc-500 text-zinc-600">
+                      plays
+                    </span>
+                  </p>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col ">
+              {year?.map((artist, index) => (
+                <Link
+                  href={artist.url}
+                  key={index}
+                  className="flex items-center justify-between p-2 duration-200 rounded-lg cursor-pointer dark:hover:bg-zinc-900/60 hover:bg-zinc-100/60 hover:shadow-lg"
+                  target="_blank"
+                >
+                  <p className="m-0 dark:text-zinc-200 text-zinc-900">
+                    {artist.name}
+                  </p>
+                  <p className="m-0 dark:text-zinc-200 text-zinc-900">
+                    {artist.playcount}{" "}
+                    <span className="dark:text-zinc-500 text-zinc-600">
+                      plays
+                    </span>
+                  </p>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <div className="flex flex-col justify-between md:w-1/2 w-full gap-2 p-4 rounded-lg shadow-xl dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200">
         <div>
           <h2 className="m-0 text-xl font-black dark:text-zinc-200 text-zinc-900">
             Top Artists
@@ -37,7 +128,7 @@ export default function TopArtists() {
               <p className="m-0 dark:text-zinc-200 text-zinc-900">
                 {artist.name}
               </p>
-              {/* <span className="text-zinc-500">{"//"}</span> */}
+
               <p className="m-0 dark:text-zinc-200 text-zinc-900">
                 {artist.playcount}{" "}
                 <span className="dark:text-zinc-500 text-zinc-600">plays</span>
@@ -64,7 +155,7 @@ export default function TopArtists() {
               <p className="m-0 dark:text-zinc-200 text-zinc-900">
                 {artist.name}
               </p>
-              {/* <span className="text-zinc-500">{"//"}</span> */}
+
               <p className="m-0 dark:text-zinc-200 text-zinc-900">
                 {artist.playcount}{" "}
                 <span className="dark:text-zinc-500 text-zinc-600">plays</span>
@@ -72,7 +163,7 @@ export default function TopArtists() {
             </Link>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
