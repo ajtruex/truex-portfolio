bc525acdcca346cb4c3621502eab37bdfc5243b2
Andrew Truex <andrewtruex@gmail.com>
2024-04-28 06:40:20 -0400

    add dropdown to toggle month/year on spotify

--- components/TopTracks.tsx
+++ components/TopTracks.tsx
@@ -2,6 +2,8 @@
 import fetcher from "../lib/fetcher"
 import useSWR from "swr"
 import Link from "next/link"
+import { useState } from "react"
+import { FiChevronDown } from "react-icons/fi"
 
 export type TopTracksResponse = {
   name: string
@@ -11,6 +13,8 @@ export type TopTracksResponse = {
 }
 
 export default function TopTracks() {
+  const [to, setTo] = useState("Month")
+  const [isOpen, setIsOpen] = useState(false)
   const { data } = useSWR<TopTracksResponse[]>("/api/top-tracks", fetcher)
   const { data: year } = useSWR<TopTracksResponse[]>(
     "/api/top-tracks-year",
@@ -24,6 +28,97 @@ export default function TopTracks() {
           <h2 className="m-0 dark:text-zinc-200 text-zinc-900 font-black text-xl">
             Top Tracks
           </h2>
+          <div className="relative">
+            <button
+              className="bg-zinc-800 p-2 rounded hover:bg-zinc-800 text-sm dark:text-zinc-400 text-zinc-700"
+              onClick={() => setIsOpen(!isOpen)}
+              value={to}
+            >
+              {to}
+              <FiChevronDown className="inline ml-1" />
+            </button>
+            {isOpen && (
+              <div className="absolute mt-1 m-0 text-sm dark:text-zinc-400 text-zinc-700 rounded p-1 shadow bg-zinc-800">
+                <button
+                  onClick={(
+                    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => (
+                    setTo((e.target as HTMLButtonElement).value),
+                    setIsOpen(false)
+                  )}
+                  className="block w-full p-2 rounded  hover:bg-gray-100"
+                  value={"Month"}
+                >
+                  Month
+                </button>
+                <button
+                  onClick={(
+                    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+                  ) => (
+                    setTo((e.target as HTMLButtonElement).value),
+                    setIsOpen(false)
+                  )}
+                  className="block w-full p-2 rounded hover:bg-gray-100"
+                  value={"Year"}
+                >
+                  Year
+                </button>
+              </div>
+            )}
+          </div>
+          {to === "Month" ? (
+            <div className="flex flex-col">
+              {data?.map((track, index) => (
+                <Link
+                  href={track.url}
+                  key={index}
+                  className="flex justify-between items-center dark:hover:bg-zinc-900/60 hover:bg-zinc-100/60 rounded-lg p-2 hover:shadow-lg cursor-pointer duration-200"
+                  target="_blank"
+                >
+                  <p className="dark:text-zinc-200 text-zinc-900 m-0 flex flex-col">
+                    {track.name}{" "}
+                    <span className="text-xs dark:text-zinc-400 text-zinc-700">
+                      {track.artist}
+                    </span>
+                  </p>
+                  <p className="dark:text-zinc-200 text-zinc-900 m-0">
+                    {track.playcount}{" "}
+                    <span className="text-zinc-500">plays</span>
+                  </p>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col ">
+              {year?.map((track, index) => (
+                <Link
+                  href={track.url}
+                  key={index}
+                  className="flex justify-between items-center dark:hover:bg-zinc-900/60 hover:bg-zinc-100/60 rounded-lg p-2 hover:shadow-lg cursor-pointer duration-200 truncate"
+                  target="_blank"
+                >
+                  <p className="dark:text-zinc-200 text-zinc-900 m-0 flex flex-col truncate">
+                    {track.name}{" "}
+                    <span className="text-xs dark:text-zinc-400 text-zinc-700">
+                      {track.artist}
+                    </span>
+                  </p>
+                  <p className="dark:text-zinc-200 text-zinc-900 m-0">
+                    {track.playcount}{" "}
+                    <span className="text-zinc-500">plays</span>
+                  </p>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <div className="dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200 rounded-lg shadow-xl p-4 flex flex-col justify-between gap-2 md:w-1/2 w-full">
+        <div>
+          <h2 className="m-0 dark:text-zinc-200 text-zinc-900 font-black text-xl">
+            Top Tracks
+          </h2>
+
           <p className="m-0 dark:text-zinc-400 text-zinc-700 text-sm">Month</p>
         </div>
         <div className="flex flex-col ">
@@ -46,8 +141,8 @@ export default function TopTracks() {
             </Link>
           ))}
         </div>
-      </div>
-      <div className="dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200 rounded-lg shadow-xl p-4 flex flex-col justify-between gap-2 md:w-1/2 w-full">
+      </div> */}
+      {/* <div className="dark:bg-gradient-to-r dark:from-neutral-900 dark:to-zinc-800 bg-gradient-to-r from-neutral-200 to-zinc-200 rounded-lg shadow-xl p-4 flex flex-col justify-between gap-2 md:w-1/2 w-full">
         <div>
           <h2 className="m-0 dark:text-zinc-200 text-zinc-900 font-black text-xl">
             Top Tracks
@@ -74,7 +169,7 @@ export default function TopTracks() {
             </Link>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
