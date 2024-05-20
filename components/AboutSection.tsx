48dcb622454d5d542e3b9e9488e0293c32c24e9d
Andrew Truex <andrewtruex@gmail.com>
2024-05-20 15:28:36 -0400

    change button and section underline colors

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -23,7 +23,7 @@ const AboutSection = () => {
       <div className="my-12 pb-12 md:pt-16 ">
         <h1 className="text-center font-bold text-4xl">
           About Me
-          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+          <hr className="w-6 h-1 mx-auto my-4 bg-gradient-to-r from-indigo-500    to-fuchsia-500 border-0 rounded"></hr>
         </h1>
         <div className="flex flex-col md:flex-row space-y-10 items-stretch justify-center align-top md:text-left md:p-4 md:space-y-0 md:space-x-10">
           <div className="md:w-1/2">
