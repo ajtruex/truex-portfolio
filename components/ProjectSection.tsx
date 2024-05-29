a4333aba38101bf6f53977d86ec8d1fbd2a870db
Andrew Truex <andrewtruex@gmail.com>
2024-05-28 21:45:11 -0400

    add command key to footer and remove slideup from projects

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -39,50 +39,50 @@ const ProjectSection = () => {
         Projects
         <hr className="w-6 h-1 mx-auto my-4 bg-gradient-to-r from-indigo-500    to-fuchsia-500 border-0 rounded"></hr>
       </h1>
-      <div className="flex flex-col space-y-28">
+      <div className="flex flex-col space-y-12">
         {projects
           .map((project, idx) => {
             return (
               <div key={idx}>
-                <SlideUp offset="-300px 0px -300px 0px">
-                  <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                    <div className="md:w-1/2 mt-8">
-                      <Link href={`/projects/${project.route}`}>
-                        <Image
-                          src={project.image}
-                          alt=""
-                          width={1000}
-                          height={1000}
-                          className="rounded-xl shadow-xl hover:opacity-70"
+                {/* <SlideUp offset="-300px 0px -300px 0px"> */}
+                <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className="md:w-1/2 mt-8">
+                    <Link href={`/projects/${project.route}`}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="md:w-1/2 mt-12">
+                    <Link href={`/projects/${project.route}`}>
+                      <h1 className="text-4xl font-bold mb-6">
+                        {project.name}
+                      </h1>
+                    </Link>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
                         />
                       </Link>
                     </div>
-                    <div className="md:w-1/2 mt-12">
-                      <Link href={`/projects/${project.route}`}>
-                        <h1 className="text-4xl font-bold mb-6">
-                          {project.name}
-                        </h1>
-                      </Link>
-                      <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                        {project.description}
-                      </p>
-                      <div className="flex flex-row align-bottom space-x-4">
-                        <Link href={project.github} target="_blank">
-                          <BsGithub
-                            size={30}
-                            className="hover:-translate-y-1 transition-transform cursor-pointer"
-                          />
-                        </Link>
-                        <Link href={project.link} target="_blank">
-                          <BsArrowUpRightSquare
-                            size={30}
-                            className="hover:-translate-y-1 transition-transform cursor-pointer"
-                          />
-                        </Link>
-                      </div>
-                    </div>
                   </div>
-                </SlideUp>
+                </div>
+                {/* </SlideUp> */}
               </div>
             )
           })
