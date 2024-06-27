eaec11a990480871231c423fb99530e90f097fe8
Andrew Truex <andrewtruex@gmail.com>
2024-06-27 00:48:51 -0400

    remove calendar icon

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Image from "next/image"
 import Link from "next/link"
-import { CalendarDays } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
